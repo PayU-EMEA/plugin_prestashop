6c7d394d4e371590c1152a864b17e4df4551ff2f
agnagna <agnieszka.wisniewska@payu.pl>
2014-10-13 16:32:13 +0200

    SessionId is an old name for orderId

--- payu/controllers/front/notification.php
+++ payu/controllers/front/notification.php
@@ -41,7 +41,7 @@ class PayUNotificationModuleFrontController extends ModuleFrontController
 				$payu->validateOrder(
 					$cart->id, (int)Configuration::get('PAYU_PAYMENT_STATUS_PENDING'),
 					$cart->getOrderTotal(true, Cart::BOTH), $payu->displayName,
-					'PayU cart ID: '.$cart->id.', sessionId: '.$payu->id_session,
+					'PayU cart ID: '.$cart->id.', orderId: '.$payu->id_session,
 					null, (int)$cart->id_currency, false, $cart->secure_key,
 					Context::getContext()->shop->id ? new Shop((int)Context::getContext()->shop->id) : null
 				);
