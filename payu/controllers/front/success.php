81b46936aa6e6525a37fd5710db7dc723d16b20a
Tomasz Regdos <tomasz.regdos@payu.pl>
2016-06-07 14:40:33 +0200

    Always redirect to order detail page

--- payu/controllers/front/success.php
+++ payu/controllers/front/success.php
@@ -18,33 +18,34 @@ class PayUSuccessModuleFrontController extends ModuleFrontController
         $payu = new PayU();
 
         $payu->id_cart = Tools::getValue('id_cart');
+        $payu->payu_order_id = $this->context->cookie->__get('payu_order_id');
+        $order_payment = $payu->getOrderPaymentBySessionId($payu->payu_order_id);
 
         if (Tools::getValue('error')) {
-            $opc = (bool)Configuration::get('PS_ORDER_PROCESS_TYPE');
-            Tools::redirect('order' . ($opc ? '-opc' : '') . '.php?error=' . Tools::getValue('error'), __PS_BASE_URI__, null, 'HTTP/1.1 301 Moved Permanently');
+            Tools::redirect($this->getRedirectLink($payu->id_cart, $order_payment['id_order']), __PS_BASE_URI__, null, 'HTTP/1.1 301 Moved Permanently');
         }
-
-        $payu->payu_order_id = $this->context->cookie->__get('payu_order_id');
-
-        $order_payment = $payu->getOrderPaymentBySessionId($payu->payu_order_id);
 
         if ($order_payment) {
             $payu->id_order = (int)$order_payment['id_order'];
             $payu->id_cart = (int)$order_payment['id_cart'];
 
-            if (Cart::isGuestCartByCartId($payu->id_cart)) {
-                $order = new Order($payu->id_order);
-                $customer = new Customer((int)$order->id_customer);
-                $redirectLink = 'index.php?controller=guest-tracking&id_order=' . $order->reference . '&email=' . urlencode($customer->email);
-            } else {
-                $redirectLink = 'index.php?controller=order-detail&id_order=' . $payu->id_order;
-            }
-
             $payu->updateOrderData();
-            Tools::redirect($redirectLink, __PS_BASE_URI__, null, 'HTTP/1.1 301 Moved Permanently');
+            Tools::redirect($this->getRedirectLink($payu->id_cart, $order_payment['id_order']), __PS_BASE_URI__, null, 'HTTP/1.1 301 Moved Permanently');
         }
 
         Tools::redirect('index.php?controller=history', __PS_BASE_URI__, null, 'HTTP/1.1 301 Moved Permanently');
+
+    }
+
+    private function getRedirectLink($id_cart, $id_order)
+    {
+        if (Cart::isGuestCartByCartId($id_cart)) {
+            $order = new Order($id_order);
+            $customer = new Customer((int)$order->id_customer);
+            return 'index.php?controller=guest-tracking&id_order=' . $order->reference . '&email=' . urlencode($customer->email);
+        } else {
+            return 'index.php?controller=order-detail&id_order=' . $id_order;
+        }
 
     }
 }
