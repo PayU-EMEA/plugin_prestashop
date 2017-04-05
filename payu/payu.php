76273bea9120246f137d9ee0828137a33d25a696
Tomasz Regdos <tomasz.regdos@payu.pl>
2017-04-05 10:13:29 +0200

    Fix save settings

--- payu/payu.php
+++ payu/payu.php
@@ -321,6 +321,7 @@ class PayU extends PaymentModule
         $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
 
         $helper->tpl_vars = array(
             'fields_value' => $this->getConfigFieldsValues(),
