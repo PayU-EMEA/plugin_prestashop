6ae1adc967bad5a6f14ca6fa02a21839e2d3f63e
Tomasz Regdos <tomasz.regdos@payu.pl>
2021-02-20 09:59:28 +0100

    Quick fix #279

--- payu/tools/sdk/openpayu.php
+++ payu/tools/sdk/openpayu.php
@@ -40,6 +40,3 @@ include_once('OpenPayU/v2/Order.php');
 include_once('OpenPayU/v2/Retrieve.php');
 include_once('OpenPayU/v2/Token.php');
 include_once('OpenPayU/v2/Shop.php');
-
-include_once('OpenPayU/Model/Shop.php');
-include_once('OpenPayU/Model/Shop/Balance.php');
